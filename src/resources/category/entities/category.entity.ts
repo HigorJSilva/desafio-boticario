8ee0e45419454ebfd4430903949b768347715efa
Higor Silva <higor.junio@hotmail.com>
2024-02-19 16:37:38 -0300

    feat: add category entity

--- src/resources/category/entities/category.entity.ts
+++ src/resources/category/entities/category.entity.ts
@@ -1,1 +1,13 @@
-export class Category {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity({ name: 'categoria' })
+export class Category {
+  @PrimaryGeneratedColumn({ name: 'categoria_id' })
+  categoriaId: number;
+
+  @Column({ name: 'nome_categoria', length: 20, nullable: true })
+  nomeCategoria?: string;
+
+  @Column({ name: 'descricao_categoria', length: 200, nullable: true })
+  descricaoCategoria?: string;
+}
