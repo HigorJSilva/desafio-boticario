f91ccf6b1c979d600f2a35c9360b529f67fd092a
Higor Silva <higor.junio@hotmail.com>
2024-02-19 16:33:46 -0300

    feat: add category module

--- src/resources/resources.module.ts
+++ src/resources/resources.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import { CategoryModule } from './category/category.module';
 
-@Module({ imports: [AuthModule, UsersModule] })
+@Module({ imports: [AuthModule, CategoryModule, UsersModule] })
+
 export class ResourcesModule {}
