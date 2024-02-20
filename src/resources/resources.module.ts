a5642cfbcb5d96ecfd9efafef26a033084d2f898
Higor Silva <higor.junio@hotmail.com>
2024-02-20 13:12:30 -0300

    feat: add address module

--- src/resources/resources.module.ts
+++ src/resources/resources.module.ts
@@ -2,7 +2,8 @@ import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { CategoryModule } from './category/category.module';
+import { AddressModule } from './address/address.module';
 
-@Module({ imports: [AuthModule, CategoryModule, UsersModule] })
+@Module({ imports: [AuthModule, CategoryModule, UsersModule, AddressModule] })
 
 export class ResourcesModule {}
