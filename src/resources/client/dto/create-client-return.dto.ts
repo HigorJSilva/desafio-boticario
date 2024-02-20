201cc1a2039a68a57868b84813fbfb95cbc74697
Higor Silva <higor.junio@hotmail.com>
2024-02-20 17:39:41 -0300

    docs: add create client return doc

--- src/resources/client/dto/create-client-return.dto.ts
+++ src/resources/client/dto/create-client-return.dto.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from '@nestjs/swagger';
 import {
   IsOptional,
   IsString,
@@ -6,36 +7,106 @@ import {
   IsDateString,
   IsPhoneNumber,
 } from 'class-validator';
+import { CreateAddressReturnDto } from 'src/resources/address/dto/create-address-return.dto';
 
 export class CreateClientReturnDto {
+  @ApiProperty({
+    description: 'Client email',
+    required: false,
+    example: 'joaosilva@email.com',
+  })
   @IsEmail()
   @IsOptional()
   email?: string;
 
+  @ApiProperty({
+    description: 'Client name',
+    required: false,
+    maximum: 15,
+    minimum: 1,
+    example: 'Joaosilva',
+  })
   @IsString()
   @Length(1, 15)
   @IsOptional()
   username?: string;
 
+  @ApiProperty({
+    description: 'Client password',
+    required: false,
+    maximum: 20,
+    minimum: 1,
+    example: 'password',
+  })
   @IsString()
   @Length(1, 20)
   @IsOptional()
   senha?: string;
 
+  @ApiProperty({
+    description: 'Client name',
+    required: true,
+    maximum: 200,
+    minimum: 1,
+    example: 'João Silva',
+  })
   @IsString()
   @Length(1, 200)
   @IsOptional()
   nome?: string;
 
+  @ApiProperty({
+    description: 'Client cpf',
+    required: false,
+    maximum: 11,
+    minimum: 11,
+    example: '64489732570',
+  })
+  @IsOptional()
+  @Length(11, 11)
+  cpf?: string;
+
+  @ApiProperty({
+    description: 'Client fone',
+    required: false,
+    example: '62984421211',
+  })
   @IsString()
   @IsPhoneNumber('BR')
   @IsOptional()
   telefone?: string;
 
+  @ApiProperty({
+    description: 'Client birth date',
+    required: false,
+    example: '2024-02-20',
+  })
   @IsDateString()
   @IsOptional()
   dataNascimento?: Date;
 
+  @ApiProperty({
+    description: 'Client adderess id',
+    required: true,
+    example: 1,
+  })
   @IsOptional()
-  enderecoId?: number;
+  enderecoId: number;
+
+  @ApiProperty({
+    description: 'Client adderess id',
+    required: true,
+    example: {
+      enderecoId: 1,
+      cep: '70863-070',
+      rua: 'rua das margaridas',
+      bairro: null,
+      cidade: 'Brasília',
+      numero: null,
+      complemento: null,
+      uf: 'DF',
+    },
+  })
+  @IsOptional()
+  endereco: CreateAddressReturnDto;
 }
