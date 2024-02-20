af31b84d54207247b3831ebb6ce9dedab5c2a343
Higor Silva <higor.junio@hotmail.com>
2024-02-19 21:18:41 -0300

    refactor: app module refacoring

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { databaseProviders } from './shared/database/ormconfig';
 import { ResourcesModule } from './resources/resources.module';
 
 @Module({
   imports: [ConfigModule.forRoot(), ...databaseProviders, ResourcesModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
