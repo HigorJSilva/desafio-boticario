8665ed1acf439adc28e8417c908e2167d8253180
Higor Silva <higor.junio@hotmail.com>
2024-02-20 13:29:31 -0300

    feat: add address dto

--- src/resources/address/entities/address.entity.ts
+++ src/resources/address/entities/address.entity.ts
@@ -1,1 +1,28 @@
-export class Address {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity({ name: 'endereco' })
+export class Address {
+  @PrimaryGeneratedColumn({ name: 'endereco_id' })
+  enderecoId: number;
+
+  @Column({ length: 9, nullable: false })
+  cep: string;
+
+  @Column({ length: 100, nullable: false })
+  rua: string;
+
+  @Column({ length: 30, nullable: true })
+  bairro?: string;
+
+  @Column({ length: 30, nullable: false })
+  cidade: string;
+
+  @Column({ length: 10, nullable: true })
+  numero?: string;
+
+  @Column({ length: 100, nullable: true })
+  complemento?: string;
+
+  @Column({ length: 2, nullable: true })
+  uf?: string;
+}
